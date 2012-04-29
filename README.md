1f54a070e8ca3565bc54327c5d8fd2341f813b74
Jerad Bitner <sirkitree@gmail.com>
2012-04-28 19:49:12 -0600

    Initial commit of structure.

---
+++ README.md
@@ -0,0 +1,2 @@
+#Yammer Fitbit Integration
+_Posts your Fitbit badges to Yammer._
---
+++ package.js
@@ -0,0 +1,18 @@
+{
+  "name" : "yammer-fitbit",
+  "description" : "Post fitbit badges to Yammer",
+  "version" : "0.0.1",
+  "author" : "Jerad Bitner <sirkitree@gmail.com>",
+  "keywords" : ["yammer", "fitbit"],
+  "main" : "lib/yammer-fitbit.js",
+  "directories" : { "lib" : "./lib" },
+  "dependencies" : {
+    "yammer" : "0.0.2",
+    "fitbit" : "0.0.0"
+  },
+  "repositories" : {
+    "type" : "git",
+    "url" : "http://github.com/sirkitree/yammer-fitbit-js.git"
+  },
+  "engines" : { "node" : ">0.2.0 <0.7.0" }
+}
