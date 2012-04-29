a44ec211588331560a9bea62ca74a1aad64c8b2f
Jerad Bitner <sirkitree@gmail.com>
2012-04-29 05:06:56 -0600

    Moved templates. Created basic config.js for reading configuration from json files. Setup some basic code in clients to read config and start OAuth.

--- fitbit-keys.template.js
+++
@@ -1,8 +0,0 @@
-{
-  "name" : "fitbit-keys",
-  "Consumer key" : "",
-  "Consumer secret" : "",
-  "Request token URL" : "http://api.fitbit.com/oauth/request_token",
-  "Access token URL" : "http://api.fitbit.com/oauth/access_token",
-  "Authorize URL" : "http://api.fitbit.com/oauth/authorize"
-}
